74d84410e7ae0f0e6ded17b92836d15bf5cee73a
Jhennifer-lincoln <jhenniferlincoln@gmail.com>
2024-10-21 19:53:16 +0000

    Definindo a página da aplicação web

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const host = "0.0.0.0";
+const porta = 3000;
+
+const app = express();
+
+function paginaInicial(requisicao, resposta) {
+    resposta.send(`<h1>Seja bem-vindo!</h1>`)
+}
+
+app.get("/", paginaInicial);
+
+app.listen(porta, host, () => {
+    console.log("Servidor em execução http://" + host + ":" + porta);
+})
